fe7991d628ddc64ca2f3f412229eabd9fe389116
huska20 <sharifka87@gmail.com>
2023-09-21 10:58:29 -0700

    the new correcction

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,29 +1,5 @@
-# Ensure the SSH client configuration directory exists for the ubuntu user
-file { '/home/ubuntu/.ssh':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0700',
-}
+# setup client SSH configuration
 
-# Configure SSH to use the private key ~/.ssh/school for the ubuntu user
-file_line { 'Configure SSH to use the private key':
-  path    => '/home/ubuntu/.ssh/config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^IdentityFile',
-  ensure  => 'present',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/home/ubuntu/.ssh'],
-}
-
-# Refuse to authenticate using a password for the ubuntu user
-file_line { 'Refuse password authentication':
-  path    => '/home/ubuntu/.ssh/config',
-  line    => 'PasswordAuthentication no',
-  match   => '^PasswordAuthentication',
-  ensure  => 'present',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require => File['/home/ubuntu/.ssh'],
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
 }
